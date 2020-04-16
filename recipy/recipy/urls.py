ab46f4251018350ba9a60591f1b9b3ea15d36695
Yılmaz BAYSAL <yilmaz.baysal@technarts.com>
2020-04-17 00:05:22 +0300

    Small tweaks.

--- recipy/recipy/urls.py
+++ recipy/recipy/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('main.urls')),
-    path(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
     path('admin/', admin.site.urls),
 ]
